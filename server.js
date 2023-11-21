c47dd1789deec4391f53d85296894d61d586889f
Gerald Ingraham <geraldingraham@Geralds-MBP.attlocal.net>
2023-11-21 17:52:50 -0500

    first commit server built. API down

---
+++ server.js
@@ -0,0 +1,36 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/dream', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const aiResponse = await openai.images.generate({
+      model: 'dall-e-2',
+      prompt,
+      n: 1,
+      size: '1024x1024',
+    });
+console.log(aiResponse)
+    const image = aiResponse.data[0].url;
+    res.send({ image });
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
+
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
